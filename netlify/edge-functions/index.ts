d11f83ca3b83cc050716ab1f15d9518fc1464cc9
Brody Critchlow <brodycritchlow@gmail.com>
2024-02-23 21:13:26 -0700

    feat(api): setup cross-checks for repo security

--- netlify/edge-functions/index.ts
+++ netlify/edge-functions/index.ts
@@ -1,5 +1,5 @@
-import { handle } from 'hono/netlify'
-import { Hono } from 'hono'
+import { Hono } from 'https://deno.land/x/hono/mod.ts'
+import { handle } from 'https://deno.land/x/hono/adapter/netlify/mod.ts'
 
 const app = new Hono()
 
@@ -8,14 +8,37 @@ app.get('/download/:repo/:owner/:version', (c) => {
   const owner = c.req.param('owner')
   const cur_version = c.req.param('version')
 
-  return c.json({ 
-    repo: repo,
-    owner: owner, 
-    version: cur_version, 
-    download_url: `https://api.github.com/repos/${owner}/${repo}/releases/tags/${cur_version}`
-  })
+  // https://api.github.com/repos/<user>/<repo>
+  // check if valid repo
+
+  const isValidRepo = (owner: string, repo: string): Promise<boolean> => {
+    const url = `https://api.github.com/repos/${owner}/${repo}`;
+    return fetch(url)
+      .then((response: Response) => response.ok);
+  };
+
+  const isWinchRepo = (owner: string, repo: string): Promise<boolean> => {
+    const url = `https://api.github.com/repos/${owner}/${repo}/contents/.winch`;
+    return fetch(url)
+      .then((response: Response) => response.ok)
+  }
+
+  return Promise.all([isValidRepo(owner, repo), isWinchRepo(owner, repo)])
+    .then(([isValid, isWinch]) => {
+      if (!isValid) {
+        return c.json({ message: 'Invalid repo' });
+      }
+      if (!isWinch) {
+        return c.json({ message: 'Not a Winch repo' });
+      }
+      return c.json({ 
+        message: 'Success!',
+        repo: repo,
+        owner: owner, 
+        version: cur_version, 
+        download_url: `https://api.github.com/repos/${owner}/${repo}/releases/tags/${cur_version}`
+      });
+    });
 })
 
-export default app as never
-
-export const GET = handle(app)
+export default handle(app)
